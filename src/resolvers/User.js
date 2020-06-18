0ca408e42ea525f31be72e9134931e7aece1f9ed
Adam Payne <ajpayne238@gmail.com>
2020-06-17 23:11:58 -0400

    User table and Chat table queries return data properly within connections. Me query added and unused blocks of code removed

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -4,35 +4,15 @@ function __resolveReference(user, context) {
 	return context.prisma.user({ id: user.id });
 }
 
-// ME helper??
-
-
-// function user(parent, args, context){
-//     return context.prisma.chats({ where: { user: parent.id } })
-// }
-
-// function friend(parent, args, context){
-//     return context.prisma.chats({ where: { friend: parent.id } })
-// }
-
-// function participants(_parent, args, context){
-//     const res = context.prisma.chat({
-//             id: _parent.id
-//     }).participants()
-//     console.log("participants Res", res)
-//     return res
-// }
-
-// function loggedIn(parent, _args, context){
-//     return context.prisma.chats({ 
-//         where: { userId: parent.id}
-//     })
-// }
+function chats(_parent, args, context){
+    const res = context.prisma.user({
+        id: _parent.id
+    }).chats()
+    console.log("chats res", res)
+    return res
+}
 
 module.exports = {
     __resolveReference,
-    // user,
-    // friend,
-    // participants,
-    // loggedIn,
+    chats,
 }
